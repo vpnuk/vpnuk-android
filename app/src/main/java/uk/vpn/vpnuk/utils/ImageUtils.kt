2bf8f6925a9e037af569784a4792e8cd03d4a93d
Arkadii <humennyi.arkadii@gmail.com>
2019-12-07 13:44:59 +0200

    add license

--- app/src/main/java/uk/vpn/vpnuk/utils/ImageUtils.kt
+++ app/src/main/java/uk/vpn/vpnuk/utils/ImageUtils.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019 VPNUK
+ * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
+ *  
+ */
+
 package uk.vpn.vpnuk.utils
 
 import android.R.attr.name
